4e65edd22be9af3bc4064b6e838aadd96bf03bbb
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-05 19:38:19 +0300

    setting the linters

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import _ from 'lodash';
 import './style.css';
 
-function component(){
-    const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-    // _ is function in the lodash package
+  // _ is function in the lodash package
 
-    element.innerHTML = _.join(['hello', 'webpack', 'its me'], ' ');
-    element.classList.add('hello');
+  element.innerHTML = _.join(['hello', 'webpack', 'its me'], ' ');
+  element.classList.add('hello');
 
-    return element
+  return element;
 }
 
 document.body.appendChild(component());
-
-
-
