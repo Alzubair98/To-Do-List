98936ec0f16408c76334635184375bc3a611587e
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-07 20:13:08 +0300

    fix linter erros & add edit function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './style.css';
-import { localStorageGet, addElement, deleteElement } from './add_remove';
+import {
+  localStorageGet, addElement, deleteElement, editElement,
+} from './add_remove.js';
 
 const arrowButtons = document.querySelector('.arrows');
 
@@ -7,36 +9,40 @@ arrowButtons.addEventListener('click', () => {
   arrowButtons.classList.toggle('active');
 });
 
-const addbutton = document.querySelector(".add-button");
-const theInput = document.querySelector(".input-bar");
+const addbutton = document.querySelector('.add-button');
+const theInput = document.querySelector('.input-bar');
 const ListSection = document.querySelector('.list-elements');
-
 
 localStorageGet();
 
 // adding to list
-addbutton.addEventListener('click', ()=>{
-  if(theInput.value !== ""){
+addbutton.addEventListener('click', () => {
+  if (theInput.value !== '') {
     addElement(theInput.value);
     theInput.value = '';
   }
 });
 
-theInput.addEventListener('keydown', (evnet)=>{
-  if(evnet.key === "Enter"){
+theInput.addEventListener('keydown', (evnet) => {
+  if (evnet.key === 'Enter') {
     addbutton.click();
   }
-} )
+});
 
 // delete from the list
 
 ListSection.addEventListener('click', (event) => {
-  if(event.target.classList.contains('delete-icon')){
+  if (event.target.classList.contains('delete-icon')) {
     event.target.parentElement.remove();
-    
-    const ID = parseInt(event.target.parentElement.getAttribute('div_id'));
-    console.log(ID)
-    deleteElement(ID);
-    
+    const iD = parseInt(event.target.parentElement.getAttribute('div_id'), 10);
+    deleteElement(iD);
+  }
+});
+
+ListSection.addEventListener('click', (event) => {
+  if (event.target.classList.contains('task-layout')) {
+    const iD = parseInt(event.target.parentElement.getAttribute('div_id'), 10);
+    const { value } = event.target;
+    editElement(iD, value);
   }
 });
