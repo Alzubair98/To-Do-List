840e76289984c31807b5f4c28ae3fa968d11dfc5
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-14 16:24:08 +0300

    more tests

--- __test__/add_remove.test.js
+++ __test__/add_remove.test.js
@@ -1,14 +1,15 @@
-import { addElement, deletetask } from '../src/add_remove.js';
+import { addElement, deletetask, deleteElement } from '../src/add_remove.js';
 
 describe('Add, Remove Element Test', () => {
   document.body.innerHTML = '<div class="list-elements"></div>';
 
-  const task = [{ description: 'text', completed: false, index: 1 }];
+  const task = { description: 'text', completed: false, index: 1 };
 
   test('check add item in the list', () => {
     addElement(task);
     const list = document.querySelectorAll('.list-elements div');
     expect(list).toHaveLength(1);
+    expect(JSON.parse(localStorage.getItem('List'))).toHaveLength(1);
   });
 
   test('check the remove from the list', () => {
@@ -17,4 +18,18 @@ describe('Add, Remove Element Test', () => {
     const list = document.querySelectorAll('.list-elements div');
     expect(list).toHaveLength(0);
   });
+
+  test('check the remove from the localStorage', () =>{
+    deleteElement(1);
+    expect(JSON.parse(localStorage.getItem('List'))).toHaveLength(0);
+  })
+
+  test('check the remove from the localStorage', () =>{
+    const tasks = [{ description: 'text', completed: false, index: 1 },
+    { description: 'two', completed: false, index: 2 },
+    { description: 'three', completed: false, index: 3 }];
+    localStorage.setItem('List', JSON.stringify(tasks))
+    deleteElement(1);
+    expect(JSON.parse(localStorage.getItem('List'))).toHaveLength(2);
+  })
 });
