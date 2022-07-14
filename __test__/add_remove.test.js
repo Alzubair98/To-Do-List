844c4a6c625f8b2e0b2b7c971c4cd7b00cbde44c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-14 17:11:24 +0300

    add comments

--- __test__/add_remove.test.js
+++ __test__/add_remove.test.js
@@ -1,10 +1,11 @@
 import { addElement, deletetask, deleteElement } from '../src/add_remove.js';
 
 describe('Add, Remove Element Test', () => {
+  // mock the html
   document.body.innerHTML = '<div class="list-elements"></div>';
 
   const task = { description: 'text', completed: false, index: 1 };
-
+  // test the add to the html and to the local storage
   test('check add item in the list', () => {
     addElement(task);
     const list = document.querySelectorAll('.list-elements div');
@@ -12,18 +13,20 @@ describe('Add, Remove Element Test', () => {
     expect(JSON.parse(localStorage.getItem('List'))).toHaveLength(1);
   });
 
+  // check the remove from the html
   test('check the remove from the list', () => {
     const deleteButton = document.querySelectorAll('.delete-icon');
     deletetask(deleteButton[0]);
     const list = document.querySelectorAll('.list-elements div');
     expect(list).toHaveLength(0);
   });
-
+  // check the remove from the localStorage
   test('check the remove from the localStorage', () => {
     deleteElement(1);
     expect(JSON.parse(localStorage.getItem('List'))).toHaveLength(0);
   });
-
+  // check the remove from the local Storage , add 3 tasks and remove one
+  // the local storage is mocked using package called jest-localstorage-mock
   test('check the remove from the localStorage', () => {
     const tasks = [{ description: 'text', completed: false, index: 1 },
       { description: 'two', completed: false, index: 2 },
